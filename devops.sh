77a06ef784cd5d2501d9e3d7c4e61ff491055c4f
fch98171-wq <fch98171@gmail.com>
2025-08-24 01:32:14 +0500

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -1,2 +1,5 @@
 echo"hello inshahALlah I am world best devoper enginer one day"
 echo"inshahALlah inshahAllah inshahALlah"
+
+echo"hello inshahALlah I am world best devoper enginer one day"
+echo"inshahALlah inshahAllah inshahALlah"
